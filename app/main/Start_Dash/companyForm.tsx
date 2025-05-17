350c8c0f2dea04c7981bb0cb57d46dada8386683
Pere Kada <joelkada16@gmail.com>
2025-05-17 18:45:31 +0100

    details form

--- app/main/Start_Dash/companyForm.tsx
+++ app/main/Start_Dash/companyForm.tsx
@@ -1,33 +1,54 @@
+import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
+import { Form } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import React from 'react';
 
 const companyForm = () => {
 	return (
-		<div>
-			<Card>
+		<div className='w-full'>
+			<Card className=' w-full border-none border-transparent shadow-none'>
+					<div className=' absolute top-2 mt-0 mb-4 '>
+						<h4 className='font-bold'> Company Details</h4>
+						<p className='text-muted-foreground'> Add company details</p>
+					</div>
 				<form>
-					<span>
-						<Label htmlFor='Industry'>Industry</Label>
+					<span className='p-6 items-start'>
+						<Label
+							className='mb-3'
+							htmlFor='Industry'>
+							Industry
+						</Label>
 						<Input
+							className='h-8 mb-0.5'
 							type='text'
 							id='Industry'
 							required
 						/>
 					</span>
-					<div className='grid grid-cols-2 gap-4'>
-						<span>
-							<Label htmlFor='CompanyEmail'>Company Email</Label>
+					<div className='grid grid-cols-2 gap-6 '>
+						<span className=' items-start'>
+							<Label
+								className='mb-3'
+								htmlFor='CompanyEmail'>
+								Company Email
+							</Label>
 							<Input
+								className='h-8 mb-4'
 								type='text'
 								id='CompanyEmail'
 								required
 							/>
 						</span>
-						<span>
-							<Label htmlFor='Companyphone'>Company phone number</Label>
+						<span className=' items-start'>
+							<Label
+								className='mb-3'
+								htmlFor='Companyphone'>
+								Company phone number
+							</Label>
 							<Input
+								className='h-8 '
 								type='text'
 								id='Companyphone'
 								required
@@ -35,24 +56,119 @@ const companyForm = () => {
 						</span>
 					</div>
 
-					<span>
-						<Label htmlFor='Address1'> Address1</Label>
+					<span className='p-6 items-start mb-3'>
+						<Label
+							className='mb-3'
+							htmlFor='Address1'>
+							{' '}
+							Address1
+						</Label>
 						<Input
+							className='h-8 mb-4'
 							type='text'
 							id='Address1'
 							required
 						/>
 					</span>
-					<span>
-						<Label htmlFor='Address1'> Address1</Label>
+					<span className=' items-start'>
+						<Label
+							className='mb-3'
+							htmlFor='Address2'>
+							{' '}
+							Address2
+						</Label>
 						<Input
+							className='h-8 mb-4'
 							type='text'
-							id='Address1'
+							id='Address2'
 							required
 						/>
 					</span>
-					<span></span>
+					<div className='grid grid-cols-2 gap-6 p-0 items-start'>
+						<span className=' items-start'>
+							<Label
+								className='mb-3'
+								htmlFor='City'>
+								City
+							</Label>
+							<Input
+								className='h-8 mb-4'
+								type='text'
+								id='City'
+								required
+							/>
+						</span>
+						<span className=' items-start'>
+							<Label
+								className='mb-3'
+								htmlFor='State'>
+								State
+							</Label>
+							<Input
+								className='h-8 mb-4'
+								type='text'
+								id='State'
+								required
+							/>
+						</span>
+					</div>
+					<div className='grid grid-cols-2 gap-6'>
+						<span className='items-start'>
+							<Label
+								className='mb-3'
+								htmlFor='Country'>
+								Country
+							</Label>
+							<Input
+								className='h-8 '
+								type='text'
+								id='Country'
+								required
+							/>
+						</span>
+						<span className='items-start'>
+							<Label
+								className='mb-3'
+								htmlFor='TIN'>
+								Tax Identification Number (TIN)
+							</Label>
+							<Input
+								className='h-8 '
+								type='text'
+								id='TIN'
+								required
+							/>
+						</span>
+					</div>
+					<span>
+						<Label
+							className='m-3'
+							htmlFor='Website'>
+							Company Website
+						</Label>
+						<Input
+							className='h-8 '
+							type='text'
+							id='Website'
+							required
+						/>
+					</span>
 				</form>
+
+				<div className='self-end'>
+					<Button
+						className='m-3 text-muted-foreground'
+						variant='outline'>
+						{' '}
+						Close{' '}
+					</Button>
+					<Button
+						className='m-3 bg-[#3D56A8] text-white '
+						variant='outline'>
+						{' '}
+						Save{' '}
+					</Button>
+				</div>
 			</Card>
 		</div>
 	);
