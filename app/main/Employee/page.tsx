102bd456eeed54b892a0c5b02f559fc3490da087
Pere Kada <joelkada16@gmail.com>
2025-05-23 14:24:11 +0100

    employee adj

--- app/main/Employee/page.tsx
+++ app/main/Employee/page.tsx
@@ -3,30 +3,27 @@ import { Button } from '@/components/ui/button'
 
 const page = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <h3 className='text-2xl font-bold, top-0.5, left-0.5'>Employee</h3>
-         
-          <p className='text-sm text-muted-foreground'>Manage your employees</p>
-          <Button variant="outline">Add Employee</Button>
-          <Button variant="outline">Import Employees</Button>
-        </div>
-        <div> 
-          <h2>
-          Add employees
-          </h2>
-          <p>
-            Add employees to your company. You can add employees one by one or import them in bulk. 
-          </p>        
-          <Button variant="outline">Add Employee</Button>
-          <Button variant="outline">Import Employees</Button>
+		<div className='self-center w-[688px] h-[603px] ml-7 gap-4'>
+			<div>
+				<div>
+					<h3 className='text-2xl font-bold,'>Employee</h3>
 
-        </div>
-
-      </div>
-    </div>
-  )
+					<p className='text-sm text-muted-foreground'>Manage your employees</p>
+					<Button variant='outline'>Add Employee</Button>
+					<Button variant='outline'>Import Employees</Button>
+				</div>
+				<div>
+					<h2>Add employees</h2>
+					<p>
+						Add employees to your company. You can add employees one by one or
+						import them in bulk.
+					</p>
+					<Button variant='outline'>Add Employee</Button>
+					<Button variant='outline'>Import Employees</Button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default page
