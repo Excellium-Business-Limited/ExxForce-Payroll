137faae2e334f339448f86af14ace81dcd998246
Pere Kada <joelkada16@gmail.com>
2025-05-25 09:09:59 +0100

    loandemo

--- app/main/Loan/loanDetails.tsx
+++ app/main/Loan/loanDetails.tsx
@@ -1,19 +1,102 @@
 import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
+import { Progress } from '@/components/ui/progress';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import Image from 'next/image';
 import React from 'react';
 
 export default function LoanDetails() {
-    return (
-			<div className=''>
-				<div className='self-center w-[688px] h-[603px] ml-7 gap-4'>
+	return (
+		<div className=''>
+			<div className='self-center w-[688px] h-[603px] ml-7 gap-4'>
+				<div>
+					<pre className='flex flex-row items-center'>
+						<h1 className='text-muted-foreground'>Loan</h1> /{' '}
+						<h1>Loan Details</h1>
+					</pre>
+					{/* <Button className='flex self-end bg-[#3D56A8]'></Button> */}
 					<div>
-						<pre className='flex flex-row items-center'>
-							<h1 className='text-muted-foreground'>Loan</h1> /{' '}
-							<h1>Loan Details</h1>
-						</pre>
-						<Button className='flex self-end bg-[#3D56A8]'></Button>
+						<Card></Card>
+						<Card></Card>
+					</div>
+					<div>
+						<Table>
+							<TableHeader>
+								<div>
+									<Progress /> 2/6 months
+								</div>
+								<div>
+									<span>
+										<h5 className='text-muted-foreground'>Amount Paid</h5>
+										<p></p>
+									</span>
+									<span>
+										<h5 className='text-muted-foreground'>Balance Remaining</h5>
+										<p></p>
+									</span>
+									<span>
+										<h5 className='text-muted-foreground'>Next Deduction</h5>
+										<p></p>
+									</span>
+								</div>
+							</TableHeader>
+							<TableHeader>
+								<TableRow>
+									<TableHead>Month</TableHead>
+									<TableHead>Amount Deducted</TableHead>
+									<TableHead>Balance Remaining</TableHead>
+									<TableHead>Date of Deduction</TableHead>
+									<TableHead>Status</TableHead>
+								</TableRow>
+							</TableHeader>
+							<TableBody>
+								<TableRow>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+									<TableCell></TableCell>
+								</TableRow>
+							</TableBody>
+						</Table>
 					</div>
 				</div>
 			</div>
-		);
+		</div>
+	);
 }
