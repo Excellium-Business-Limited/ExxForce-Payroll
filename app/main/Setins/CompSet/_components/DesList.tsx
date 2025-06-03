15c8dd6bbbc9f581a589395b4ae627f6e3b4e245
Pere Kada <joelkada16@gmail.com>
2025-06-03 17:59:19 +0100

    designation and department

---
+++ app/main/Setins/CompSet/_components/DesList.tsx
@@ -0,0 +1,85 @@
+import LoanForm from '@/app/main/Loan/loanForm';
+import { Button } from '@/components/ui/button';
+import {
+	Sheet,
+	SheetContent,
+	SheetTitle,
+	SheetTrigger,
+} from '@/components/ui/sheet';
+import {
+	Table,
+	TableHeader,
+	TableRow,
+	TableHead,
+	TableBody,
+	TableCell,
+} from '@/components/ui/table';
+import React from 'react';
+
+const DesList = () => {
+	return (
+		<div>
+			<section className='flex justify-between align-middle px-4 py-2'>
+				<h4>Designation</h4>
+				<span className='items-end self-end justify-between flex gap-4'>
+					<Sheet>
+						<SheetTrigger asChild>
+							<Button
+								variant={'outline'}
+								className='bg-[#3D56A8] text-white'>
+								+ Add Designation
+							</Button>
+						</SheetTrigger>
+						<SheetContent className='min-w-[500px] p-4 overflow-auto'>
+							<SheetTitle className='hidden'></SheetTitle>
+							Add Designation
+						</SheetContent>
+					</Sheet>
+				</span>
+			</section>
+			<Table>
+				<TableHeader>
+					<TableRow>
+						<TableHead>Designation</TableHead>
+						<TableHead>Total Employees</TableHead>
+						<TableHead>Actions</TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					<TableRow>
+						<TableCell>Software Developer</TableCell>
+						<TableCell></TableCell>
+						<TableCell></TableCell>
+					</TableRow>
+					<TableRow>
+						<TableCell>Technical Writer</TableCell>
+						<TableCell></TableCell>
+						<TableCell></TableCell>
+					</TableRow>
+					<TableRow>
+						<TableCell>Product Designer</TableCell>
+						<TableCell></TableCell>
+						<TableCell></TableCell>
+					</TableRow>
+					<TableRow>
+						<TableCell>Legal Officer</TableCell>
+						<TableCell></TableCell>
+						<TableCell></TableCell>
+					</TableRow>
+					<TableRow>
+						<TableCell>Project Manager</TableCell>
+						<TableCell></TableCell>
+						<TableCell></TableCell>
+					</TableRow>
+					<TableRow>
+						<TableCell>Cloud Engineer</TableCell>
+						<TableCell></TableCell>
+						<TableCell></TableCell>
+					</TableRow>
+				</TableBody>
+			</Table>
+		</div>
+	);
+};
+
+export default DesList;
--- app/main/Setins/CompSet/page.tsx
+++ app/main/Setins/CompSet/page.tsx
@@ -1,24 +1,35 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import CompList from './_components/compList';
 import React from 'react';
+import DesList from './_components/DesList'
 import { Card } from '@/components/ui/card';
+import DepList from './_components/DepList';
 
 const page = () => {
 	return (
 		<div className='h-[680px]'>
 			<div className='mx-4'>
 				<h1>Company Settings</h1>
-				<h5 className='text-sm'>Manage your organization settings and preferences</h5>
+				<h5 className='text-sm'>
+					Manage your organization settings and preferences
+				</h5>
 			</div>
 			<div className=' bg-white rounded-lg h-[433px] m-5'>
-				<Tabs className='self-center' defaultValue='Company'>
+				<Tabs
+					className='self-center'
+					defaultValue='Company'>
 					<TabsList className='no-design'>
 						<div className='m-2'>
-				<TabsTrigger
-					value='Company'
-					className='data-[state=active]:text-[#3d56a8] data-[state=active]:underline text-muted-foreground'>
-					Company
-				</TabsTrigger>
+							<TabsTrigger
+								value='Company'
+								className='data-[state=active]:text-[#3d56a8] data-[state=active]:underline text-muted-foreground'>
+								Company
+							</TabsTrigger>
+							<TabsTrigger
+								value='Designation'
+								className='data-[state=active]:text-[#3D56A8] data-[state=active]:underline text-muted-foreground'>
+								Designation
+							</TabsTrigger>
 							<TabsTrigger
 								value='Department'
 								className='data-[state=active]:text-[#3D56A8] data-[state=active]:underline text-muted-foreground'>
@@ -30,7 +41,10 @@ const page = () => {
 					<TabsContent value='Company'>
 						<CompList />
 					</TabsContent>
-					<TabsContent value='Department'></TabsContent>
+					<TabsContent value='Designation'>
+						<DesList />
+					</TabsContent>
+					<TabsContent value='Department'><DepList/></TabsContent>
 				</Tabs>
 			</div>
 		</div>
