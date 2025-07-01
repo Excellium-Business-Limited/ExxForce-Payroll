7c6b099967f157be415fe81b99bd9b260b6e5b48
Perekada <59916970+Perekada@users.noreply.github.com>
2025-07-01 16:14:52 +0100

    manage payrun

--- app/main/Payrun/paytemps/page.tsx
+++ app/main/Payrun/paytemps/page.tsx
@@ -5,11 +5,55 @@ import React from 'react'
 const page = () => {
   return (
 		<div>
-			<section>
-				<Card></Card>
-				<Card></Card>
-				<Card></Card>
-				<Card></Card>
+			<section className='grid grid-rows-1 grid-cols-4 gap-4 m-3'>
+				<Card>
+					<img
+						src=''
+						alt=''
+					/>
+					<h5>Total Employees </h5>
+					<hr />
+					<span>
+						<h4>50</h4>
+						<p>90% of employees are regular staff</p>
+					</span>
+				</Card>
+				<Card>
+					<img
+						src=''
+						alt=''
+					/>
+					<h5>Total Net Salary</h5>
+					<hr />
+					<span>
+						<h4>₦3,500,000.00</h4>
+						<p>Total payroll after deductions</p>
+					</span>
+				</Card>
+				<Card>
+					<img
+						src=''
+						alt=''
+					/>
+					<h5>Total Deduction</h5>
+					<hr />
+					<span>
+						<h4>₦3,500,000.00</h4>
+						<p>Deduction from all employees in this run</p>
+					</span>
+				</Card>
+				<Card>
+					<img
+						src=''
+						alt=''
+					/>
+					<h5>Prorated Salaries</h5>
+					<hr />
+					<span>
+						<h4>15 Employees</h4>
+						<p>30% of employees had prorated pay</p>
+					</span>
+				</Card>
 			</section>
 			<section>
 				<Card>
@@ -27,19 +71,19 @@ const page = () => {
 								<TableCell>MORE</TableCell>
 							</TableRow>
 						</TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-              </TableRow>
-            </TableBody>
+						<TableBody>
+							<TableRow>
+								<TableCell></TableCell>
+								<TableCell></TableCell>
+								<TableCell></TableCell>
+								<TableCell></TableCell>
+								<TableCell></TableCell>
+								<TableCell></TableCell>
+								<TableCell></TableCell>
+								<TableCell></TableCell>
+								<TableCell></TableCell>
+							</TableRow>
+						</TableBody>
 					</Table>
 				</Card>
 			</section>
